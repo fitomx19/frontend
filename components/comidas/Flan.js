9a2ce38d668f86960a7135eb2b3be72a1fda5dfa
Adolfo Huerta <fitox.metallica@gmail.com>
2023-12-14 21:22:18 -0600

    avance proyecto fnal

--- components/comidas/Flan.js
+++ components/comidas/Flan.js
@@ -1,11 +1,53 @@
 import React from 'react'
+import Azucar from './imagenes/sugar.png';
+import Huevo from './imagenes/huevo.png';
+import Leche from './imagenes/leche.png';
+import Image from 'next/image';
 
 const Flan = () => {
     return (
-        <div>
-            <h1>Flan</h1>
+        <div className="max-w-4xl mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-4">Flan</h1>
+            <p className="text-lg mb-2">Ingredientes:</p>
+            <div className="overflow-x-auto">
+                <table className="min-w-full bg-white border border-gray-300">
+                    <thead>
+                        <tr className="bg-gray-100">
+                            <th className="px-6 py-3 text-left">PRODUCTO</th>
+                            <th className="px-6 py-3 text-left">PRESENTACI�N</th>
+                            <th className="px-6 py-3 text-left">PRECIO PROMEDIO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={Azucar} alt="Azucar" className="w-8 h-8 mr-2" />
+                                AZUCAR
+                            </td>
+                            <td className="px-6 py-4">1 KG. Estandar</td>
+                            <td className="px-6 py-4">42.03</td>
+                        </tr>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={Huevo} alt="Huevo" className="w-8 h-8 mr-2" />
+                                HUEVO
+                            </td>
+                            <td className="px-6 py-4">1 KG.</td>
+                            <td className="px-6 py-4">51.13</td>
+                        </tr>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={Leche} alt="Leche" className="w-8 h-8 mr-2" />
+                                LECHE ULTRAPASTEURIZADA
+                            </td>
+                            <td className="px-6 py-4">1 Lt.</td>
+                            <td className="px-6 py-4">29.36</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Flan
--- pages/about.js
+++ pages/about.js
@@ -19,6 +19,38 @@ const About = () => {
         <p className="mb-4">
           El Mapa de la Comida utiliza datos abiertos de la Ciudad de México para mostrar los precios de locales de comida y supermercados en la ciudad.
         </p>
+
+        <p className="text-lg font-semibold mb-4">
+          ¿Cual es la fórmula para calcular la calificación de los locales?
+        </p>
+        <p className="mb-4">
+          La fórmula para calcular la calificación de los locales es la siguiente:
+        </p>
+        <p className="mb-4">
+          - Distancia más larga de un local a un supermercado: 0 puntos
+        </p>
+        <p className="mb-4">
+        - Distancia más corta de un local a un supermercado: 10 puntos
+        </p>
+        <p className="mb-4">
+          Precio más alto de la cuenta de un supermercado: 0 puntos
+        </p>
+        <p className="mb-4">
+          Precio más bajo de la cuenta de un supermercado: 10 puntos
+        </p>
+        <p className="mb-4">
+          Indice de Desarrollo Humano de la alcaldía: Hasta 10 puntos
+        </p>
+        <p className="mb-4">
+          Precio por metro cuadrado más alto de la alcaldía: 0 puntos
+        </p>
+        <p className="mb-4">
+          Precio por metro cuadrado más bajo de la alcaldía: 10 puntos
+        </p>
+
+        <p className="text-lg font-semibold mb-4">
+          Sumamos los puntos de cada local y los dividimos entre 5 para obtener la calificación.
+        </p>
       </div>
       <Footer />
     </>
